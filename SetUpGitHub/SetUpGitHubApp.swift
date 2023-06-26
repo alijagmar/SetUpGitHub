3fdf2ec981450d889fe0b83fba050b6b37f8dbc5
Alia Marzyie <marzyiealia@gmail.com>
2023-06-26 10:12:16 -0400

    Initial Commit

---
+++ SetUpGitHub/SetUpGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetUpGitHubApp.swift
+//  SetUpGitHub
+//
+//  Created by Alia Marzyie on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct SetUpGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
